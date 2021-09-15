fc37f565cc4cc5b93866a82de3674b0296d7e6bf
Elio <eliobricenov@gmail.com>
2021-09-14 23:27:11 -0400

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,34 @@
+import React from "react";
+import { useRouter } from "next/router";
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import theme from "../src/theme";
 import { CssBaseline, ThemeProvider } from "@material-ui/core";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	const router = useRouter();
+	const { route } = router;
+
+	React.useEffect(() => {
+		// Remove the server-side injected CSS.
+		const jssStyles = document.querySelector("#jss-server-side");
+		if (jssStyles) {
+			jssStyles.parentElement?.removeChild(jssStyles);
+		}
+
+		const handleRouteChange = (url: URL) => {};
+
+		//When the component is mounted, subscribe to router changes
+		//and log those page views
+		router.events.on("routeChangeComplete", handleRouteChange);
+
+		// If the component is unmounted, unsubscribe
+		// from the event with the `off` method
+		return () => {
+			router.events.off("routeChangeComplete", handleRouteChange);
+		};
+	}, [router.events]);
+
 	return (
 		<ThemeProvider theme={theme}>
 			{/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
