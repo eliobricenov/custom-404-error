ebdcc2cec3e6e48ff8a46287714956cbc538d61c
Elio <eliobricenov@gmail.com>
2021-09-14 23:11:10 -0400

    feat: add material UI

---
+++ src/theme.ts
@@ -0,0 +1,15 @@
+// Create a theme instance.
+import { createTheme } from "@material-ui/core";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+  },
+});
+
+export default theme;
