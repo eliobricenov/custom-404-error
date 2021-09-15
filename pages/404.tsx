eeff9874d1d8f13b0db793768827ccf64903c0e2
Elio <eliobricenov@gmail.com>
2021-09-14 21:16:22 -0400

    feat: add 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage(): JSX.Element {
+    return (
+        <div style={{width: '100%', display: 'flex', justifyContent: 'center'}}>
+            <h1>Page Not Found :(</h1>
+        </div>
+    )
+}
